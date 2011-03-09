0eef53ecd7c2cbbf8f0e67f19ea156812ea9d4d6
Brendan Berg <brendan@plusminusfive.com>
2011-03-09 00:19:32 +0000

    Modified test.py to actually, you know, work

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 	# parser = OptionsParser()
 	tornado.options.define('port', default=5000, type=int, 
 		metavar='PORT', help='set which port on which the application will listen')
-	tornado.options.define('address', default="", type=string, 
+	tornado.options.define('address', default="", type=str, 
 		metavar='ADDR', help='address of the interface the server binds to')
 	tornado.options.parse_command_line()
 	
